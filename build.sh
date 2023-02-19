da2623baa443491b87cb28829c77899b7f6c56eb
Aryan Github <34622095+Aryanamish@users.noreply.github.com>
2023-02-19 13:50:47 +0530

    added echo in build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,22 @@
 set -o errexit
 
-python --version
-python3 --version
-
+echo "Removing poetry"
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 - --uninstall
+echo "Installing poetry"
 curl -sSL https://install.python-poetry.org | python3 -
 
+echo "Updating poetry"
 poetry self update
 
+echo "Setting up python venv"
 poetry install
 
+echo "Collect Static"
 python manage.py collectstatic --no-input
+echo "Migrate DataBase"
 python manage.py migrate
+
+echo "Building Frontend"
 cd frontend
 npm i
 npm prune
