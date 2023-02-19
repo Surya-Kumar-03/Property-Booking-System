539cdddf3607cc3b477c2f0e5110c8c2cff1639b
Aryan Github <34622095+Aryanamish@users.noreply.github.com>
2023-02-19 13:40:24 +0530

    Update build.sh to update potery
    update potery

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 set -o errexit
 
+python --version
+python3 --version
+
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 - --uninstall
+curl -sSL https://install.python-poetry.org | python3 -
+
 poetry self update
+
 poetry install
 
 python manage.py collectstatic --no-input
